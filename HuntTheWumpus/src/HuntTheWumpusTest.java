c1d41e993fc5d42a5b306400588a46d2290bbd2a
Abhishek Rane <rane_abhishek@me.com>
2014-09-08 15:41:23 -0700

    Added first test

--- HuntTheWumpus/src/HuntTheWumpusTest.java
+++ HuntTheWumpus/src/HuntTheWumpusTest.java
@@ -1,4 +1,7 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Point;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
@@ -6,8 +9,17 @@ import org.junit.Test;
 public class HuntTheWumpusTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSlimeLogic() {
+		Map map = new Map();
+		ArrayList<Point> pitPoints = new ArrayList<Point>();
+		Point point = new Point(2,2);
+		pitPoints.add(point);
+		map.setPits(pitPoints);
+		assertTrue(map.getCell(point).getPit());
+		assertTrue(map.getCell(new Point(1,2)).getSlime());
+		assertTrue(map.getCell(new Point(3,2)).getSlime());
+		assertTrue(map.getCell(new Point(2,1)).getSlime());
+		assertTrue(map.getCell(new Point(2,3)).getSlime());
+		System.out.println(map.toStringDebug());
 	}
-
 }
