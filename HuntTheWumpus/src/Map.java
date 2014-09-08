465f6dbc9dd8ff9ae2b3ae0effc6387d2656326a
Abhishek Rane <rane_abhishek@me.com>
2014-09-08 15:40:16 -0700

    Started test

--- HuntTheWumpus/src/Map.java
+++ HuntTheWumpus/src/Map.java
@@ -11,7 +11,7 @@ public class Map {
 		map = new Cell[10][10];
 		for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
-				map[i][j] = new Cell();n
+				map[i][j] = new Cell();
 			}
 		}
 	}
