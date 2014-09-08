32539a29419c5396af289eeb09bc7277fec83af6
Abhishek Rane <rane_abhishek@me.com>
2014-09-08 15:44:22 -0700

    Updated game runner to have the game runner

--- HuntTheWumpus/src/GameRunner.java
+++ HuntTheWumpus/src/GameRunner.java
@@ -1,4 +1,19 @@
 // test pull
 public class GameRunner {
-
+	private Map gameMap;
+	/*
+	 * Default Constructor. Initializes a random map.
+	 */
+	public GameRunner(){
+		this.gameMap = new Map();
+		gameMap.generatePits();
+	}
+	
+	/*
+	 * Primary run loop for the game. 
+	 */
+	public void run(){
+		
+	}
+	
 }
