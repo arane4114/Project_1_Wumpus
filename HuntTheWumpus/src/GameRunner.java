c6011b1552942b6c128c4f9b77e44d04515e937b
Bryce Hammond <Bryce Hammond@BryceHammond-PC>
2014-09-08 17:43:02 -0700

    HardUpdatingMyLocal

--- HuntTheWumpus/src/GameRunner.java
+++ HuntTheWumpus/src/GameRunner.java
@@ -1,4 +1,25 @@
 // test pull
 public class GameRunner {
-
+	private Map gameMap;
+	/*
+	 * Default Constructor. Initializes a random map.
+	 */
+	public GameRunner(){
+		this.gameMap = new Map();
+		gameMap.generateWumpus();
+		gameMap.generatePits();
+	}
+	
+	/*
+	 * Primary run loop for the game. 
+	 */
+	public void run(){
+		System.out.println(gameMap.toStringDebug());
+	}
+	
+	
+	public static void main(String[] args){
+		GameRunner game = new GameRunner();
+		game.run();
+	}
 }
