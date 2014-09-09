5abdf241966ea493cc3bbafc8b6e4ca4328d550c
Abhishek Rane <rane_abhishek@me.com>
2014-09-09 13:23:45 -0700

    Working version. Need to add more tests, comments, and error handleing.

--- HuntTheWumpus/src/Cell.java
+++ HuntTheWumpus/src/Cell.java
@@ -8,109 +8,67 @@ public class Cell {
 	private boolean slime;
 	private boolean blood;
 	private boolean pit;
-	private boolean wumpus; 
+	private boolean wumpus;
 	private boolean hunter;
-	
-	
-//	public Cell(Boolean hiddenRoom, Boolean slime, Boolean blood, Boolean pit, Boolean wumpus) {
-//		this.hiddenRoom = hiddenRoom;
-//		this.slime = slime;
-//		this.blood = blood;
-//		this.pit = pit;
-//		this.wumpus = wumpus;
-//		
-//	}
-	
-	public Cell(){
+
+	public Cell() {
 		this.hiddenRoom = true;
 	}
-	
-	public boolean getHiddenRoom(){
+
+	public boolean getHiddenRoom() {
 		return hiddenRoom;
 	}
-	
-	public boolean getSlime(){
+
+	public boolean getSlime() {
 		return slime;
 	}
-	
-	public boolean getBlood(){
+
+	public boolean getBlood() {
 		return blood;
 	}
-	
-	public boolean getPit(){
+
+	public boolean getPit() {
 		return pit;
 	}
-	
-	public boolean getNothing(){
-		return !slime && !blood && !pit;
+
+	public boolean getIsEmpty() {
+		return !slime && !blood && !pit && !wumpus;
 	}
-	
-	public boolean getWumpus(){
+
+	public boolean getWumpus() {
 		return wumpus;
 	}
-	
-	public boolean getGoop(){
+
+	public boolean getGoop() {
 		return slime && blood;
 	}
-	
-	public boolean getHunter(){
+
+	public boolean getHunter() {
 		return hunter;
 	}
-	
-	public void setHiddenRoom(Boolean hiddenRoom){
+
+	public void setHiddenRoom(Boolean hiddenRoom) {
 		this.hiddenRoom = hiddenRoom;
 	}
-	
-	public void setSlime(Boolean slime){
+
+	public void setSlime(Boolean slime) {
 		this.slime = slime;
 	}
-	
-	public void setBlood(Boolean blood){
+
+	public void setBlood(Boolean blood) {
 		this.blood = blood;
 	}
-	
-	public void setPit(Boolean pit){
+
+	public void setPit(Boolean pit) {
 		this.pit = pit;
 	}
-	
-	public void setWumpus(Boolean wumpus){
+
+	public void setWumpus(Boolean wumpus) {
 		this.wumpus = wumpus;
 	}
 
-	
-	public void setHunter(boolean hunter){
+	public void setHunter(boolean hunter) {
 		this.hunter = hunter;
 	}
-	
-	
-	
-	
-	
-	// Change to what you want it to say
-	public String whatsThere(){
-		String whatsThere;
-		if(getGoop()){
-			whatsThere = "Goop";
-		}else if(getBlood()){
-			whatsThere = "Blood";
-		}else if(getSlime()){
-			whatsThere = "Slime";
-		}else{
-			whatsThere = "Nothing";
-		}
-	return whatsThere;
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
